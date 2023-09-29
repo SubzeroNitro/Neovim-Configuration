017102814664453e933d20d09bea63f82ff1fc3b
David <drvillase@gmail.com>
2023-09-29 13:29:54 -0400

    Added theme selector plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -3,6 +3,14 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	{
+		"zaldih/themery.nvim",
+		opts = {
+			themes = {},
+			themeConfigFile = "~/.config/nvim/lua/config/theme.lua",
+			livePreview = true
+		}
+	}
+	{
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			icons_enabled = true,

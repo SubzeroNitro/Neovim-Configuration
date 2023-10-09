a035e24a7d0c8cc51b6303ace845ac178038375b
David <drvillase@gmail.com>
2023-10-09 03:19:14 -0400

    New plugins added

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,19 +1,40 @@
 return {
-	{
-		"nvim-tree/nvim-web-devicons",
-	},
-	{
-		"zaldih/themery.nvim",
-		opts = {
-			themes = {},
-			themeConfigFile = "~/.config/nvim/lua/config/theme.lua",
-			livePreview = true
-		}
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = {
-			icons_enabled = true,
-		}
+    {
+	"nvim-tree/nvim-web-devicons",
+    },
+    {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+	    { 
+		"williamboman/mason.nvim",
+		config = true
+	    },
+	    {
+		"williamboman/mason-lspconfig.nvim"
+	    },
+	    {
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		opts = {}
+	    },
+	    {
+		"folke/neodev.nvim",
+		opts = {}
+	    }
 	}
+    },
+    {
+	"zaldih/themery.nvim",
+	opts = {
+	    themes = {},
+	    themeConfigFile = "~/.config/nvim/lua/config/theme.lua",
+	    livePreview = true
+	}
+    },
+    {
+	"nvim-lualine/lualine.nvim",
+	opts = {
+	    icons_enabled = true,
+	}
+    }
 }

8d9c81482c4be64b04e23c80f0844860bb77952f
David <drvillase@gmail.com>
2023-10-09 01:01:23 -0400

    Configured Vim options

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
 local core = require("core")
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.autoread = false
+vim.opt.breakindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = "menuone,preview,noselect"
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "line,number"
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+
 core.utilities.bootstrap(core.plugins)

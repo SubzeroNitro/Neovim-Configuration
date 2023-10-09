306d5ceb90a1177d54b414b1f3fde0677692af69
David <drvillase@gmail.com>
2023-10-09 04:15:27 -0400

    Configured for Neovide GUI

--- init.lua
+++ init.lua
@@ -18,4 +18,20 @@ vim.opt.smartindent = true
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
 
+if vim.g.neovide then
+    vim.g.neovide_cursor_animation_length = 0
+
+    vim.g.neovide_fullscreen = true
+
+    vim.g.neovide_padding_top = 5
+    vim.g.neovide_padding_bottom = 5
+    vim.g.neovide_padding_left = 5
+    vim.g.neovide_padding_right = 5
+
+    vim.g.neovide_hide_mouse_when_typing = true
+
+    vim.o.guifont = "RobotoMono Nerd Font:h16"
+    vim.o.linespacing = 0
+end
+
 core.utilities.bootstrap(core.plugins)
